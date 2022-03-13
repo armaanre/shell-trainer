25163836395b6617cbefaa105d1fcb60c6dd18fa
Duong Dinh <dd.dinh11@gmail.com>
2022-03-13 17:46:40 +1100

    Added ebdjango

--- ebdjango/ebdjango/wsgi.py
+++ ebdjango/ebdjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shellTrainer project.
+WSGI config for ebdjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shellTrainer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebdjango.settings')
 
 application = get_wsgi_application()
--- ebdjango/manage.py
+++ ebdjango/manage.py
@@ -2,18 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import signal
 
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    sys.exit(0)
 
 def main():
-    """Capture signal interrupt"""
-    signal.signal(signal.SIGINT, signal_handler)
-
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shellTrainer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebdjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
